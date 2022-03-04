dcefdc7c590e43551280773316fb6f21a98f26c1
p22cobrien <p22cobrien@fairfieldprep.org>
2022-03-04 10:03:21 -0500

    update

--- lab_2-2.py
+++ lab_2-2.py
@@ -1,7 +1,11 @@
 # Author: CMOB 3/4/2022
 
 def build_vehicle(p1,p2,p3,p4):
-    print("The car has {0} wheels, {1} axels, {2} doors, and is {3}".format(p1,p2,p3,p4))
+    wheels = p1
+    axels = p2
+    doors = p3
+    color = p4
+    print("The car has {0} wheels, {1} axels, {2} doors, and is {3}".format(wheels,axels,doors,color))
 
 
 wheels = input("How many wheels are there? ")
